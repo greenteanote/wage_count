b9db6f2c50f1b2297cea87eaef3f7f6ed4b3b655
greenteanote <41660915+greenteanote@users.noreply.github.com>
2025-09-17 16:55:27 +0900

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/wage_count/', // <-- IMPORTANT: Change 'your-repo-name' to your actual GitHub repository name
-})
